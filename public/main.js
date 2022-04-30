a4fe6f08c9287b0f10bc61dcc2abf4064111965c
Rohan Shankar <roshank@live.unc.edu>
2022-04-30 00:56:58 -0400

    maybe

--- public/main.js
+++ public/main.js
@@ -21,7 +21,7 @@ async function flipCoin() {
 				    document.getElementById("result").innerHTML = result.flip;
 				    document.getElementById("quarter").setAttribute("src", "assets/img/"+result.flip+".png");
 				  });
-  };
+  }
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
 // The flip many coins form in div#multi 
